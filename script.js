57277a2f6f2c49a6adf07ced221bc3ceca4ea8f3
prathibha1212 <105058182+prathibha1212@users.noreply.github.com>
2025-09-08 18:06:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let weather={
+     "apiKey":"ae98d1cf12487e8b58c70116a99465ff", 
+     fetchWeather:function(city){
+        fetch(
+            "https://pro.openweathermap.org/data/2.5/weather?q="
+        +city
+        +"&units=metric&APPID="
+        +this.apiKey
+        )
+        .then((response)=>response.json())
+        .then((data)=>this.displayWeather(data));
+     },
+     displayWeather: function(data){
+        const{name}=data;
+        const{icon,description}= data.weather[0];
+        const{temp,humidity}= data.main;
+        const{speed}= data.wind;
+        
+        document.querySelector(".city").innerText= " Weather in " + name;
+        document.querySelector(".icon").src = "http://openweathermap.org/img/wn/03d@2x.png";
+        document.querySelector(".description").innerText= description;
+        document.querySelector(".temp").innerText= temp + "°C";
+        document.querySelector(".humidity").innerText= "Humidity:" + humidity + "%";
+        document.querySelector(".wind").innerText = "wind speed:" + speed + " km/h ";
+        document.querySelector(".weather").classList.remove("loading");
+        document.body.style.backgroundImage="url('https://source.unsplash.com/1600x900/?" + name +"')"
+    },
+    search:function(){
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+    };
+    document.querySelector(".search button")
+    .addEventListener("click",function(){
+        weather.search();
+     });
+     document.querySelector(".search-bar").addEventListener("keyup",function(event){
+        if(event.key == "Enter") {
+            weather.search();
+        }
+     });
+     weather.fetchWeather("Delhi");
